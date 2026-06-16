cd694f57940df82db855efcde66e5ff8cdc09d9f
Ademar de Souza Novaes Junior <ademar.junior@havan.com.br>
2026-06-16 17:37:18 -0300

    configurado tela de menu conforme rota e formatação de componente

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ import Layout from './components/Layout';
 import RemindersPage from './pages/LembreteTodo';
 import Ferramentas from './pages/Ferramentas';
 import BannerEditor from './pages/BannerEditor';
+import { Build, NotificationsActive, ViewQuilt, Assignment } from '@mui/icons-material';
+
+const menuItems = [
+    // { text: 'Painel', icon: <Dashboard />, path: '/' },
+    { text: 'Nova Ocorrência', icon: <Assignment />, path: '/cadastro-ocorrencia' },
+    { text: 'Configuração de Ocorrências', icon: <Build />, path: '/cadastro-tipo' },
+    { text: 'Ferramentas', icon: <Build />, path: '/ferramentas' },
+    { text: 'Lembretes', icon: <NotificationsActive />, path: '/lembretes' },
+    { text: 'Editor de Banner', icon: <ViewQuilt />, path: '/banner-editor' },
+];
 
 export default function App() {
   return (
@@ -12,7 +22,7 @@ export default function App() {
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Layout menuItems={menuItems} />}>
             <Route path="/ferramentas" element={<Ferramentas />} />
             <Route path="/lembretes" element={<RemindersPage />} />
             <Route path="/banner-editor" element={<BannerEditor />} />
